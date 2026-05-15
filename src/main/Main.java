b844140708ec7a705682f542f6658b0ede3cdb7c
Miguel Herrera <maherrera_115@cue.edu.co>
2026-05-14 20:56:53 -0500

    pequeño ajuste en el main

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,9 +2,6 @@ package main;
 
 import app.App;
 
-// PUNTO DE ENTRADA DEL PROGRAMA
-// Solo llama a App para que ejecute el flujo completo
-// Así el main queda limpio y simple
 public class Main {
 
     public static void main(String[] args) {
